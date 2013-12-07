afb77e5f7b50085390db7aa3f79b8ca779a16516
Adya <adya_a@mail.ru>
2013-12-07 20:32:25 +0200

    All base playing functionality implemented.
    P.S. for testing use path "sdcard/Music/test.mp3", otherwise it won't play.

--- YAMPProject/YAMP/src/main/java/com/yamp/core/AudioManager.java
+++ YAMPProject/YAMP/src/main/java/com/yamp/core/AudioManager.java
@@ -1,29 +1,139 @@
 package com.yamp.core;
 
+import com.yamp.events.NewTrackLoadedListener;
+import com.yamp.events.PlayingCompletedListener;
+import com.yamp.events.PlayingStartedListener;
+import com.yamp.events.SoundControllerBoundedListener;
+import com.yamp.library.AudioFile;
 import com.yamp.library.PlayList;
+import com.yamp.sound.SoundController;
+
+import java.util.ArrayList;
 
 /**
  * Created by AdYa on 24.11.13.
- *
+ * <p/>
  * Responses for common operations. (Activity?)
  */
 public class AudioManager {
-  private boolean looped;
-  private boolean shuffle;
-  private PlayList trackList; // target playlist
 
-///TODO: implementation needed!!!!
 
-  public void play(){}
-  public void pause(){}
-  public void stop(){}
-  public void next(){}
-  public void prev(){}
+    private boolean looped;
+    private boolean shuffle;
+    private PlayList trackList; // target playlist
 
-  public void setVolum (float volume){}
+    /// TODO: TESTING THINGS....REPLACE IT WITH EXISTED TRACK
+    private AudioFile current = new AudioFile("/sdcard/Music/test.mp3");
 
-  public void setPlayList(PlayList playlist){
-      if (playlist != null && playlist.size() > 0)
-        this.trackList = playlist;
-  }
+
+    private SoundController controller;
+    private static AudioManager instance;
+    private boolean readyToPlay = false;
+
+    public static AudioManager getInstance() {
+        if (instance == null) instance = new AudioManager();
+        return instance;
+    }
+
+    private AudioManager() {
+        instance = this;
+        YAMPApplication.setOnSoundControllerBoundedListener(new SoundControllerBoundedListener() {
+            @Override
+            public void onSoundControllerBounded(SoundController controller) {
+               AudioManager.this.controller = controller;
+            }
+        });
+        trackList = new PlayList();
+        trackList.addTrack(current);
+    }
+
+    private void setTrack(AudioFile track){
+        String name = track.getName();
+        readyToPlay = true;
+        current = track;
+        if (controller.isPlaying())
+            controller.play(name); ///TODO: Change getName() to getPath()
+        else if (controller.isPaused())
+            controller.setTrack(name);
+        else
+
+        fireNewTrackLoaded(track);
+    }
+
+
+    public void play() {
+        if (!readyToPlay){
+            controller.play(current.getName()); ///TODO: Change getName() to getPath()
+            readyToPlay = true;
+        }
+        else controller.play();
+        fireNewTrackLoaded(current);
+    }
+    public void pause() {
+        controller.pause();
+    }
+    public void stop() {
+        controller.stop();
+    }
+
+    public void next() {
+        setTrack(trackList.nextTrack());
+    }
+    public void prev() {
+       setTrack(trackList.prevTrack());
+    }
+
+    public void seekTo(int msec) {
+        controller.seekTo(msec);
+    }
+    public int getDuration() {
+        return controller.getDuration();
+    }
+
+    public int getVolumeMax(){
+        return SoundController.MAX_VOLUME;
+    }
+    public int getVolume(){
+        return controller.getVolume();
+    }
+    public void setVolume(int volume) {
+        controller.setVolume(volume);
+    }
+
+    public void setPlayList(PlayList playlist) {
+        if (playlist != null && playlist.size() > 0)
+            this.trackList = playlist;
+    }
+
+    public boolean isPlaying() {
+        return controller.isPlaying();
+    }
+
+    public void setLooping(boolean looped) {
+        controller.setLooping(looped);
+    }
+
+
+    /**
+     * Listeners ....
+     **/
+
+    private ArrayList<NewTrackLoadedListener> newTrackLoadedListeners = new ArrayList<>();
+    private void fireNewTrackLoaded(AudioFile track){
+        for (NewTrackLoadedListener listener : newTrackLoadedListeners){
+            listener.onNewTrackLoaded(track);
+        }
+    }
+    public void setOnNewTrackLoadedListener(NewTrackLoadedListener listener){
+        newTrackLoadedListeners.add(listener);
+    }
+
+    // SoundController redirection
+    public void setOnPlayingStartedListener(PlayingStartedListener listener) {
+        controller.setOnPlayingStartedListener(listener);
+    }
+    public void setOnPlayingCompletedListener(PlayingCompletedListener listener) {
+        controller.setOnPlayingCompletedListener(listener);
+    }
+
 }
---
+++ YAMPProject/YAMP/src/main/java/com/yamp/events/PlayingCompletedListener.java
@@ -0,0 +1,8 @@
+package com.yamp.events;
+
+/**
+ * Created by AdYa on 03.12.13.
+ */
+public interface PlayingCompletedListener {
+    public void onPlayingCompleted();
+}
